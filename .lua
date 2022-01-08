80d4bd59fbb24dbf833412d6359fc79111309023
NgocThinh310 <82268879+NgocThinh310@users.noreply.github.com>
2022-01-08 11:25:48 +0700

    Update .lua

--- .lua
+++ .lua
@@ -1,3 +1,2 @@
-\45\45\32\80\117\116\32\115\99\114\105\112\116\32\104\101\114\101\10
-Put the above encoded string into the loadstring below between the quotation marks for the obfuscated scrippts.
-loadstring("")()
+
+loadstring("\45\45\32\80\117\116\32\115\99\114\105\112\116\32\104\101\114\101\10")()
